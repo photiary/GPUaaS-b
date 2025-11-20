9c62b8030d4a3eb2d970b1e4d94020f21eec81ad
funa <photiary@gmail.com>
2025-11-20 14:49:27 +0900

    init

---
+++ src/test/java/com/funa/gpuaasb/GpUaaSBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funa.gpuaasb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpUaaSBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
